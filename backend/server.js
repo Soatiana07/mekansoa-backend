bfaa2ea5deaa7c96c4ee78deb529a282c86d97ef
Soatiana07 <99266931+Soatiana07@users.noreply.github.com>
2025-04-03 14:51:21 +0300

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -15,9 +15,9 @@ app.use(cors({
 }));
 
 app.use((req, res, next) => {
-    // res.header("Access-Control-Allow-Origin", "*");
-    // res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization, X-Connecte");
-    // res.header("Access-Control-Allow-Methods", "GET,PUT,POST,DELETE,OPTIONS");
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization, X-Connecte");
+    res.header("Access-Control-Allow-Methods", "GET,PUT,POST,DELETE,OPTIONS");
     res.header("Access-Control-Expose-Headers", "X-Connecte, Authorization");
 
     // console.log("Headers envoyés dans la réponse :", res.getHeaders()); // Vérifie si X-Connecte est bien ajouté
