799ab57029557ef07d052b6b78060403e3f043a2
Soatiana07 <99266931+Soatiana07@users.noreply.github.com>
2025-04-03 16:59:33 +0300

    Update authClientMiddleware.js

--- backend/middlewares/authClientMiddleware.js
+++ backend/middlewares/authClientMiddleware.js
@@ -1,7 +1,7 @@
 const jwt = require('jsonwebtoken');
 
 module.exports = async (req, res, next) => {
-    try { 
+    // try { 
         console.log("tonga aty zaaaaaa");
         console.log("req.path :", req.path);
         
@@ -20,21 +20,29 @@ module.exports = async (req, res, next) => {
             return res.status(401).json({ message: 'Accès refusé. Aucun token fourni.' });
         }
 
-        const decoded = jwt.verify(token, process.env.JWT_SECRET || 'mekansoa');
-        console.log('Decoded : ', decoded);
         
-        req.client = decoded;
+        jwt.verify(token, process.env.JWT_SECRET, (err, decoded) => {
+            if (err) {
+                if(err.name === 'TokenExpiredError'){
+                    return res.status(401).json({ message: 'Token expiré.' });
+                }       
+            }
+            req.client = decoded;
+            next();
+        });
+        
+        
         // res.setHeader('X-Connecte', '0');  
         // console.log('Header X-Connecte ajoutéeeeeee:', res.getHeaders('X-Connecte'));
 
-        next();
-    } catch (error) {
-        // res.setHeader('X-Connecte', '1');  
-        if (error.name === 'TokenExpiredError') {
-            return res.status(401).json({ message: 'Token expiré.' });
-        }
-        return res.status(401).json({ message: 'Token invalide ou expiré.' });
-    }
+        // next();
+    // } catch (error) {
+    //     // res.setHeader('X-Connecte', '1');  
+    //     if (error.name === 'TokenExpiredError') {
+    //         return res.status(401).json({ message: 'Token expiré.' });
+    //     }
+    //     return res.status(401).json({ message: 'Token invalide ou expiré.' });
+    // }
 };
 
 // const jwt = require('jsonwebtoken');
