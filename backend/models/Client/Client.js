cca6c0cdc84ecce8272ed516dd01ab627aa50f5e
Soatiana07 <99266931+Soatiana07@users.noreply.github.com>
2025-04-03 12:56:26 +0300

    Update bcrypt

--- backend/models/Client/Client.js
+++ backend/models/Client/Client.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
-const bcrypt = require('bcrypt');
+const bcrypt = require('bcrypts');
 
 const ClientSchema = new mongoose.Schema({
     nom: { type: String, required: true },
